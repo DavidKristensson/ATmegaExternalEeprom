02b6d65cb6cc2280fc810c46571d44f043ad1adf
David <David.Kristensson@hotmail.com>
2020-11-05 23:32:51 +0100

    Sub-Task 2: Writing DAVID char by char to EEPROM and reading it back from EEPROM to print it through UART.

--- source/main.c
+++ source/main.c
@@ -12,25 +12,44 @@
 #include "timer.h"
 
 int main(void) {
-
-	uint8_t eepromOutputByte;
 	/*
 		Initalizing I2C and UART
 
 		Enabling global interrupts
 
-		Writing a char to EEPROM
-		Reading char from EEPROM and saving in variable and printing it
+		Writing DAVID char by char to EEPROM
+		Printing DAVID through UART by reading from EEPROM
 	*/
 	i2c_init();
 	uart_init();
 	sei();
 
+	/*
+	// Sub-task 1
+	uint8_t eepromOutputByte;
 	eeprom_write_byte(EEPROM_ADDR_WRITE, 'D');
+	*/
+
+	uint8_t counter = 0;
+	eeprom_write_byte(EEPROM_ADDR_WRITE, 'D');
+	eeprom_write_byte(EEPROM_ADDR_WRITE + 1, 'A');
+	eeprom_write_byte(EEPROM_ADDR_WRITE + 2, 'V');
+	eeprom_write_byte(EEPROM_ADDR_WRITE + 3, 'I');
+	eeprom_write_byte(EEPROM_ADDR_WRITE + 4, 'D');
 	while (1) {
+		printf_P(PSTR("%c"), eeprom_read_byte(EEPROM_ADDR_WRITE + counter));
+		counter++;
+		if (counter == 5) {
+			counter = 0;
+			printf_P(PSTR("\n"));
+		}
+		
+		/*
+		// Sub-Task 1
 		eepromOutputByte = eeprom_read_byte(EEPROM_ADDR_WRITE);
 
 		printf_P(PSTR("%c\n"), eepromOutputByte);
+		*/
 	}
 	return 0;
 }
